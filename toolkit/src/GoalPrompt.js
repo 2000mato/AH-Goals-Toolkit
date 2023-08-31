c81e8c55bbd075e3770a0b18299929f1384181db
Bill Tomasello <billytomasello1@gmail.com>
2023-08-31 13:09:11 -0400

    default end date is three months from now

--- toolkit/src/GoalPrompt.js
+++ toolkit/src/GoalPrompt.js
@@ -8,7 +8,10 @@ import "./GoalPrompt.css";
 function GoalPrompt() {
     // add a usestate method later based on clicking a button to reset the goal or intitialize the goal
     const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(null); // If you need an end date as well
+    const currentDate = new Date();
+    const threeMonthsFromNow =  new Date(currentDate.setMonth(currentDate.getMonth() + 3));
+
+    const [endDate, setEndDate] = useState(threeMonthsFromNow); // If you need an end date as well
 
 
     return (
@@ -18,10 +21,9 @@ function GoalPrompt() {
             <input type="text" placeholder = "Make it slightly more lofty than you think you can achieve. Without stress, growth does not occur" />
             <div>What is the major block of action required to achieve this goal</div>
             <input type = "text" placeholder ="I am going to x 4x/week , for periods of 30 minutes, 15 of which are spent doing y" />
-            <div>For how long will you pursue this goal, on what days and for how long?</div>
+            <div>For how long will you pursue this goal, on what days and for how long?(It is advised to use a period of roughly 12 weeks, or 3 months)</div>
             <div>
             <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-            {/* If you need an end date as well */}
             <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
         </div>
         </div>
