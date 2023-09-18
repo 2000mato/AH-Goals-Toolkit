ffecf09f61dcee97f86b3482c60ff2151a0fa2f4
YBilly Tomasello <billytomasello1@gmail.com>
2023-09-18 11:35:48 -0400

    user table and goal table created

---
+++ backend/users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    hashed_password VARCHAR(255) NOT NULL,
+    last_login TIMESTAMP
+)
+
+CREATE TABLE goals (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERNCES users(id),
+    goal_description TEXT NOT NULL,
+    action_block TEXT NOT NULL,
+    start_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP + INTERVAL '3 months'
+)
