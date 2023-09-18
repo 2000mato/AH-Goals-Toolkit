b8be92b3d239cb5a68c802d0a551ba625e803949
YBilly Tomasello <billytomasello1@gmail.com>
2023-09-18 14:04:25 -0400

    database succesfully seeded, next step is to send data from the server

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,18 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const env = require('dotenv').config();
-
+const { Pool } = require('pg');
 const app = express();
+const port = process.env.PORT || 8000;
 
-const port = 8000;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  });
+
 
 // Middleware
 app.use(helmet());
@@ -17,9 +25,16 @@ app.use(morgan('combined'));
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('*Loud buzzer noise* try again');
+    }
 });
+
 
 app.listen(port, () => {
     console.log('server is listening on port ' + port);
--- backend/users.sql
+++ backend/users.sql
@@ -1,16 +1,16 @@
-CREATE TABLE users (
+CREATE TABLE "users" (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     hashed_password VARCHAR(255) NOT NULL,
     last_login TIMESTAMP
-)
+);
 
-CREATE TABLE goals (
+CREATE TABLE "goals" (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERNCES users(id),
+    user_id INTEGER REFERENCES users(id),
     goal_description TEXT NOT NULL,
     action_block TEXT NOT NULL,
     start_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     end_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP + INTERVAL '3 months'
-)
+);
